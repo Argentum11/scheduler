7126b5c3d6aacf334fdb83e85a0180ee37f212d8
AnChen_Lu <47174822+Sunrise610006@users.noreply.github.com>
2022-11-30 20:34:43 +0800

    Add coroutine_int.h comment

--- src/coroutine_int.h
+++ src/coroutine_int.h
@@ -9,22 +9,25 @@ typedef int (*job_t)(struct context *__context, void *args);
 /*
  * task_struct maintain the coroutine or task object.
  */
-struct task_struct {
+struct task_struct
+{
     /* job information */
-    struct cr *cr;
-    int tfd; /* task fd */
-    job_t job;
-    void *args;
+    struct cr *cr;          // coroutine
+    int tfd;                /* task fd */
+    job_t job;              // job
+    void *args;             // args
     struct context context; /* defined at context.h */
 
     /* default info */
-    struct {
-        struct rb_node node;
-        long sum_exec_runtime;
-        long exec_start;
+    struct
+    {
+        struct rb_node node;   // 紅黑樹節點
+        long sum_exec_runtime; // 總執行時間
+        long exec_start;       // 開始執行時間
     };
 };
 
+/* 定義container_of 並防止使用延展功能時gcc提出警告*/
 #ifndef container_of
 #define container_of(ptr, type, member)                        \
     __extension__({                                            \
@@ -33,6 +36,7 @@ struct task_struct {
     })
 #endif
 
+/* task_of 主要以 __context、task_srtuct 及 context 執行 container_of*/
 #define task_of(__context) container_of(__context, struct task_struct, context)
 
 /* runqueue */
@@ -40,43 +44,46 @@ struct task_struct {
 // Need to be power of two
 #define RINGBUFFER_SIZE 16
 
-struct rq {
-    unsigned int out, in; /* dequeue at out, enqueue  at in*/
-    unsigned int mask; /* the size is power of two, so mask will be size - 1 */
-    struct task_struct *r[RINGBUFFER_SIZE];
+struct rq
+{
+    unsigned int out, in;                   /* dequeue at out, enqueue  at in*/
+    unsigned int mask;                      /* the size is power of two, so mask will be size - 1 */
+    struct task_struct *r[RINGBUFFER_SIZE]; // runqueue 的陣列
 };
 
-void rq_init(struct rq *rq);
-int rq_enqueue(struct rq *rq, struct task_struct *task);
-struct task_struct *rq_dequeue(struct rq *rq);
+void rq_init(struct rq *rq);                             // 初始化 runqueue
+int rq_enqueue(struct rq *rq, struct task_struct *task); // 進入 runqueue 的函式
+struct task_struct *rq_dequeue(struct rq *rq);           // 離開 runqueue 的函式
 
-struct task_struct *rq_dequeue_lifo(struct rq *rq);
+struct task_struct *rq_dequeue_lifo(struct rq *rq); // lifo 函式
 
 /* main data structure */
 
-#define MAX_CR_TABLE_SIZE 10
+#define MAX_CR_TABLE_SIZE 10 // corourine 的 table 最大大小為 10
 
-struct cr {
-    unsigned long size; /* number of the task in this scheduler */
-    int crfd; /* coroutine fd number */
-    int flags; /* Which type of scheduler, FIFO or CFS */
+struct cr
+{
+    unsigned long size;          /* number of the task in this scheduler */
+    int crfd;                    /* coroutine fd number */
+    int flags;                   /* Which type of scheduler, FIFO or CFS */
     struct task_struct *current; /* the job currently working */
 
     /* scheduler - chose by the flags */
-    struct rq rq; /* FIFO */
+    struct rq rq;        /* FIFO */
     struct rb_root root; /* Default */
 
     /* sched operations */
-    int (*schedule)(struct cr *cr, job_t func, void *args);
-    struct task_struct *(*pick_next_task)(struct cr *cr);
-    int (*put_prev_task)(struct cr *cr, struct task_struct *prev);
+    int (*schedule)(struct cr *cr, job_t func, void *args);        // schedule 函式
+    struct task_struct *(*pick_next_task)(struct cr *cr);          // 定義task_struct，其中含 cr 結構
+    int (*put_prev_task)(struct cr *cr, struct task_struct *prev); // put_prev_task 函式
 };
 
-struct cr_struct {
-    int size;
-    struct cr *table[MAX_CR_TABLE_SIZE];
+struct cr_struct
+{
+    int size;                            // 大小
+    struct cr *table[MAX_CR_TABLE_SIZE]; // coroutine 的 table
 };
 
-void sched_init(struct cr *cr);
+void sched_init(struct cr *cr); // 初始化排程法的 function
 
 #endif /* __COROUTINE_INT_H__ */
